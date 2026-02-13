d4f5735714dd8d86703c9319dc34117cd7085e3a
Azamat Mercury <111359649+zhanbol1@users.noreply.github.com>
2026-02-13 14:38:11 +0600

    reaally I swear this will be deployed

--- wordle/settings.py
+++ wordle/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
