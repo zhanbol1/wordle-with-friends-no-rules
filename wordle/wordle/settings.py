e7be6b93409b805d1c1af184db5c0d7e70c49a8c
Azamat Mercury <111359649+zhanbol1@users.noreply.github.com>
2026-02-13 12:36:32 +0600

    deploy

--- wordle/wordle/settings.py
+++ wordle/wordle/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!79=&sipelx-ayb%b7uovcuvd24kexh7a$20)7eg)ywkwrv#@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
